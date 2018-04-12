6c67574434970c4d05d0a59fd220ee2e188dc835
Luis Pujols <luispujols@hotmail.com>
2018-04-12 13:29:53 -0400

    Added the base template files for the project.

---
+++ LinkedInClient/LinkedInClient/Platforms/Android/LinkedInClientImplementation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.LinkedInClient
+{
+    /// <summary>
+    /// Interface for $safeprojectgroupname$
+    /// </summary>
+    public class LinkedInClientImplementation : ILinkedInClient
+    {
+    }
+}
