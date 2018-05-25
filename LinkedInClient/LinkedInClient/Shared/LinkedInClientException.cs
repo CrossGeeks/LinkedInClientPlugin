e83d441142bc0bacfcaaba0a0a76bc329e097839
Luis Pujols <luispujols@hotmail.com>
2018-05-25 07:04:38 -0400

    Added the LinkedInClientException implementation to the project.

---
+++ LinkedInClient/LinkedInClient/Shared/LinkedInClientException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Plugin.LinkedInClient.Shared
+{
+	public class  LinkedInClientBaseException : Exception
+    {
+        public static string SignInDefaultErrorMessage = "The LinkedIn Sign In could not complete it's process correctly.";
+        public static string ApiHelperErrorMessage = "The LinkedIn Sign In could not complete it's process correctly.";
+
+        public LinkedInClientBaseException() : base() { }
+        public LinkedInClientBaseException(string message) : base(message) { }
+        public LinkedInClientBaseException(string message, System.Exception inner) : base(message, inner) { }
+    }
+
+    // Indicates an error with the API has occured.
+    public class LinkedInClientApiHelperErrorException : LinkedInClientBaseException
+    {
+        public LinkedInClientApiHelperErrorException() : base(ApiHelperErrorMessage) { }
+        public LinkedInClientApiHelperErrorException(string message) : base(message) { }
+        public LinkedInClientApiHelperErrorException(string message, System.Exception inner) : base(message, inner) { }
+    }
+}
