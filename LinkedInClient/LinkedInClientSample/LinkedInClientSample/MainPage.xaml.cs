9192e6262e1dbcedc32e7e243d6852348a9ab541
Luis Pujols <luispujols@hotmail.com>
2018-07-15 14:47:39 -0400

    Added the base files for the LinkedInClientSample App

---
+++ LinkedInClient/LinkedInClientSample/LinkedInClientSample/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LinkedInClientSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
